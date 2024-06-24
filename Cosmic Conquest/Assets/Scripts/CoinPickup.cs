026653f3a335af3ac3e50412972e61217b794349
ae-24 <115100656+ae-24@users.noreply.github.com>
2024-06-24 18:07:48 +0800

    Revert "Fixed bug with pickup coin sfx"
    This reverts commit f7ebfe57f52a240a42d4defd29b901b2b56a51f7.

--- Cosmic Conquest/Assets/Scripts/CoinPickup.cs
+++ Cosmic Conquest/Assets/Scripts/CoinPickup.cs
@@ -6,25 +6,10 @@ using UnityEngine.InputSystem.Android;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
-<<<<<<< Updated upstream
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Player") {
             AudioSource.PlayClipAtPoint(coinPickupSFX, new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z));
-=======
-    [SerializeField] int audioVolume = 50;
-    [SerializeField] int pointsForCoinPickup = 100;
-    AudioSource coinSound;
-    bool wasCollected = false;
-    void OnTriggerEnter2D(Collider2D other) 
-    {
-        if (other.tag == "Player" && !wasCollected) {
-            wasCollected = true;
-            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
-            coinSound = GameObject.Find("SFX AudioSource").GetComponent<AudioSource>();
-            coinSound.PlayOneShot(coinPickupSFX, audioVolume);
-            gameObject.SetActive(false);
->>>>>>> Stashed changes
             Destroy(gameObject);
         }    
     }
