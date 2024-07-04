53aa9209691b46d049d7aae71d61f29090644927
nicholascxh <02nicholaschong@gmail.com>
2024-07-04 15:33:41 +0800

    new game / continue button

--- Cosmic Conquest/Assets/Scripts/MainMenu.cs
+++ Cosmic Conquest/Assets/Scripts/MainMenu.cs
@@ -6,37 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // private int sceneToContinue;
+    private int sceneToContinue;
     public void PlayGame()
     {
-        // sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        SceneManager.LoadSceneAsync(1);
+        FindObjectOfType<GameSession>().playerLives = 3;
+        FindObjectOfType<GameSession>().score = 0;
+        FindObjectOfType<GameSession>().scoreText.enabled = true;
+        FindObjectOfType<GameSession>().scoreText.text = FindObjectOfType<GameSession>().score.ToString();
         
-        // if(sceneToContinue != 0) 
-        // {
-        //     SceneManager.LoadSceneAsync(sceneToContinue);
-        // }
-        // else 
-        // {
-            SceneManager.LoadSceneAsync(1);
-            FindObjectOfType<GameSession>().playerLives = 3;
-            FindObjectOfType<GameSession>().score = 0;
-            FindObjectOfType<GameSession>().scoreText.enabled = true;
-            FindObjectOfType<GameSession>().scoreText.text = FindObjectOfType<GameSession>().score.ToString();
-        // }
         
+    }
+
+    public void ContinueGame()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        
+            if(sceneToContinue != 0) 
+            {
+            SceneManager.LoadSceneAsync(sceneToContinue);
+            }
+            
     }
 
     public void Quit() 
     {
-//if (UNITY_EDITOR || DEVELOPMENT_BUILD)
-    Debug.Log(this.name+" : "+this.GetType()+" : "+System.Reflection.MethodBase.GetCurrentMethod().Name); 
-//endif
-//if (UNITY_EDITOR)
-    //UnityEditor.EditorApplication.isPlaying = false;
-//elif (UNITY_STANDALONE) 
     Application.Quit();
-//elif (UNITY_WEBGL)
     Application.OpenURL("about:blank");
-//endif
-  }
+    }
 }
